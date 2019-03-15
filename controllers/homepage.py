97dae3d6c6fd1f625861e1f70c8b691d7f2e124b
“chenhu <350951061@qq.com>
2019-03-16 00:01:13 +0800

    初学研究

--- controllers/homepage.py
+++ controllers/homepage.py
@@ -4,5 +4,5 @@ from django.shortcuts import render
 import requests
 def index(request):
     context = {}
-
+    context['a']=request.GET['a']
     return render(request, "homepage/index.html", context)

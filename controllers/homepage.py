cf49b0325b4eca4cda2eb837272885e69de2782d
“chenhu <350951061@qq.com>
2019-03-18 22:30:20 +0800

    添加字典是否在关键字中判断

--- controllers/homepage.py
+++ controllers/homepage.py
@@ -4,5 +4,7 @@ from django.shortcuts import render
 import requests
 def index(request):
     context = {}
-    context['a']=request.GET['a']
+    if "a" in request.GET:#判断一个关键字是否在字典中
+        context['query'] = request.GET['a']
+
     return render(request, "homepage/index.html", context)
